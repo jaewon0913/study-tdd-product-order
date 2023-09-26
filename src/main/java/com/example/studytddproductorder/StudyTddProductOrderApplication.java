8bf3437889152763a5b961fd6da10c7f9b039acf
jaewon <jaewon9937@gmail.com>
2023-09-26 21:47:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/studytddproductorder/StudyTddProductOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.studytddproductorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTddProductOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTddProductOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studytddproductorder/StudyTddProductOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studytddproductorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTddProductOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
