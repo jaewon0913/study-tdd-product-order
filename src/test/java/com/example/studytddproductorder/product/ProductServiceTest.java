f0aa44ac5b9451c490f75aec7ebf435d82a9a9e2
jaewon <jaewon9937@gmail.com>
2023-09-30 15:52:23 +0900

    Feat(PRODUCT_ORDER) : 상품 조회 기능 구현하기
    - 상품 조회 기능 추가
    - TestCode 추가

--- src/test/java/com/example/studytddproductorder/product/ProductServiceTest.java
+++ src/test/java/com/example/studytddproductorder/product/ProductServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 public class ProductServiceTest {
 
@@ -11,18 +13,17 @@ public class ProductServiceTest {
     private ProductService productService;
 
     @Test
-    void 상품등록() {
-        final AddProductRequest request = 상품등록요청_생셩();
+    void 상품조회() {
+        //  상품등록
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+        final long productId = 1L;
 
-        productService.addProduct(request);
-    }
+        //  상품 조회
+        final GetProductResponse response = productService.getProduct(productId);
 
-    private AddProductRequest 상품등록요청_생셩() {
-        final String name = "상품명";
-        final int price = 1000;
-        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        //  상품 응답 검증
+        assertThat(response).isNotNull();
 
-        return new AddProductRequest(name, price, discountPolicy);
     }
 
 }
---
+++ src/test/java/com/example/studytddproductorder/product/ProductServiceTest_Section1.java
@@ -0,0 +1,28 @@
+package com.example.studytddproductorder.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceTest_Section1 {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void 상품등록() {
+        final AddProductRequest request = 상품등록요청_생셩();
+
+        productService.addProduct(request);
+    }
+
+    private AddProductRequest 상품등록요청_생셩() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+
+        return new AddProductRequest(name, price, discountPolicy);
+    }
+
+}
