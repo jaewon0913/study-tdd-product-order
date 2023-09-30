7f8144f37a3b58a5ed456a377fc6e3fa8b79aa85
jaewon <jaewon9937@gmail.com>
2023-09-30 14:46:00 +0900

    Feat(PRODUCT_ORDER) : JPA 적용하기
    - JPA 전환
    - Entity 수정
    - DB TestCode 추가

--- src/test/java/com/example/studytddproductorder/product/ProductServiceTest.java
+++ src/test/java/com/example/studytddproductorder/product/ProductServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.studytddproductorder.product;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
