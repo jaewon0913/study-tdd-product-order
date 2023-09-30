2a1e3f97c59bc01e0f1bd9458ce26081920d9aff
jaewon <jaewon9937@gmail.com>
2023-09-30 13:30:33 +0900

    Feat(PRODUCT_ORDER) : API 테스트 전환하기
    - Rest-Assured Dependency 추가
    - API TestCode 추가

---
+++ src/test/java/com/example/studytddproductorder/product/ProductApiTest.java
@@ -0,0 +1,45 @@
+package com.example.studytddproductorder.product;
+
+import com.example.studytddproductorder.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ProductApiTest extends ApiTest {
+
+    @Test
+    void 상품등록() {
+        final var request = 상품등록요청_생셩();
+
+        // API 요청
+        final var response = 상품등록요청(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+    }
+
+    private AddProductRequest 상품등록요청_생셩() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+
+        return new AddProductRequest(name, price, discountPolicy);
+    }
+
+}
