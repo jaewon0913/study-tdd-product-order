82908a90fc05777d20a6dfc0894e064aeccc393e
jaewon0913 <jaewon9937@gmail.com>
2023-10-01 16:26:50 +0900

    Feat(PRODUCT_ORDER) : API 테스트로 전환하기
    - 상품 수정 기능 API 테스트로 전환

--- src/test/java/com/example/studytddproductorder/product/ProductApiTest.java
+++ src/test/java/com/example/studytddproductorder/product/ProductApiTest.java
@@ -1,14 +1,21 @@
 package com.example.studytddproductorder.product;
 
 import com.example.studytddproductorder.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ProductApiTest extends ApiTest {
 
     private final ProductSteps productSteps = new ProductSteps();
+    @Autowired
+    private ProductRepository productRepository;
 
     @Test
     void 상품등록() {
@@ -31,5 +38,28 @@ public class ProductApiTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 
         assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+    }
+
+    @Test
+    void 상품수정() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = 상품수정요청(productId);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        assertThat(productRepository.findById(1L).get().getName()).isEqualTo("상품 수정");
+    }
+
+    private static ExtractableResponse<Response> 상품수정요청(final Long productId) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(ProductSteps.상품수정요청_생성())
+                .when()
+                .patch("/products/{productId}", productId)
+                .then().log().all()
+                 .extract();
     }
 }
