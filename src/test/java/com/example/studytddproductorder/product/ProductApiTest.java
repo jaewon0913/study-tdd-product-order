27b2907e26f56020f2b45e4c890477f495cc3969
jaewon0913 <jaewon9937@gmail.com>
2023-10-01 16:00:48 +0900

    Feat(PRODUCT_ORDER) : POJO 상품 수정 기능 구현하기
    - POJO 방식 상품 수정 기능 추가
    - TestCode 추가
    - Domain TestCode 추가

--- src/test/java/com/example/studytddproductorder/product/ProductApiTest.java
+++ src/test/java/com/example/studytddproductorder/product/ProductApiTest.java
@@ -1,9 +1,6 @@
 package com.example.studytddproductorder.product;
 
 import com.example.studytddproductorder.ApiTest;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -29,11 +26,7 @@ public class ProductApiTest extends ApiTest {
 
         Long productId = 1L;
 
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .get("/products/{productId}", productId)
-                .then().log().all()
-                .extract();
+        final var response = ProductSteps.상품조회요청(productId);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 
--- src/test/java/com/example/studytddproductorder/product/ProductSteps.java
+++ src/test/java/com/example/studytddproductorder/product/ProductSteps.java
@@ -26,4 +26,12 @@ public class ProductSteps {
 
         return new AddProductRequest(name, price, discountPolicy);
     }
+
+    public static ExtractableResponse<Response> 상품조회요청(final Long productId) {
+        return RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+    }
 }
